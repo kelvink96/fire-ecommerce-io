e15a180bc1ab2253a2b3b52e3ca2fed89049ecac
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2022-03-12 19:41:26 +0300

    # Cart Page - posting order to firebase logic complete - bug fix on calculating total price - NaN error because of commas in price value

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,23 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import Layout from "../components/Layout";
 import {useDispatch, useSelector} from "react-redux";
-import {Button, Card, Image, Table} from "react-bootstrap";
-import {BsTrash} from "react-icons/bs";
+import {Button, Card, Form, Image, Modal, Spinner, Table, Toast, ToastContainer} from "react-bootstrap";
+import {BsCheck2Circle, BsExclamationCircle, BsTrash} from "react-icons/bs";
+import {addDoc, collection} from "firebase/firestore";
+import firebaseDB from "../firebase.config";
 
 const CartPage = () => {
 	const {cartItems} = useSelector(state => state.cartReducer);
 	const dispatch = useDispatch();
 	const [totalAmount, setTotalAmount] = useState(0);
 	const [loading, setLoading] = useState(false);
+	const [showToasts, setShowToast] = useState({show: false, message: '', title: ''});
+	const [show, setShow] = useState(false);
+	const [name, setName] = useState('');
+	const [address, setAddress] = useState('');
+	const [pinCode, setPinCode] = useState('');
+	const [phoneNumber, setPhoneNumber] = useState('');
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
 	useEffect(() => {
 		setLoading(true);
 		localStorage.setItem("cartItems", JSON.stringify(cartItems));
 		let temp = 0;
-
-		cartItems.forEach(c => temp += Number(c.price.split('KSh')[1]));
+		cartItems.forEach(c => temp += Number(c.price.split('KSh')[1].trim().split(",").join("")));
 		setTotalAmount(temp);
-		console.log(totalAmount);
 		setLoading(false);
 	}, [cartItems]);
 
@@ -25,12 +34,34 @@ const CartPage = () => {
 		dispatch({type: "DELETE_FROM_CART", payload: product})
 	}
 
+	const handlePlaceOrder = () => {
+		const addressInfo = {
+			name, address, pinCode, phoneNumber
+		};
+
+		const orderPayload = {
+			cartItems,
+			addressInfo,
+			email: JSON.parse(localStorage.getItem('currentUser')).user.email,
+			id: JSON.parse(localStorage.getItem('currentUser')).user.uid,
+		}
+
+		setLoading(true);
+		addDoc(collection(firebaseDB, "orders"), orderPayload).then(resp => {
+			setLoading(false);
+			setShowToast({show: true, type: 'success', message: 'order placement successful', title: 'success'});
+		}).catch(err => {
+			setLoading(false);
+			setShowToast({show: true, type: 'error', message: err.message, title: 'error'});
+		})
+	}
+
 	return (
-		<Layout loading={loading}>
+		<Layout>
 			<Card>
 				<Card.Body>
 					<Table responsive>
-						<thead>
+						<thead className="table-secondary">
 						<tr>
 							<th>image</th>
 							<th>item</th>
@@ -58,9 +89,72 @@ const CartPage = () => {
 						</tr>
 						</tfoot>
 					</Table>
-					<Card.Footer className="bg-transparent border-0 float-end"><Button>place order</Button></Card.Footer>
+					<Card.Footer className="bg-transparent border-0 float-end">
+						<Button onClick={handleShow}>Place order</Button>
+					</Card.Footer>
 				</Card.Body>
 			</Card>
+			<Modal show={show} onHide={handleClose} centered>
+				<Modal.Header closeButton>
+					<Modal.Title>Submit order</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<Form.Group className="mb-3">
+							<Form.Label>name</Form.Label>
+							<Form.Control type="text" placeholder="enter name..."
+														onChange={(evt) => setName(evt.target.value)}/>
+						</Form.Group>
+						<Form.Group className="mb-3">
+							<Form.Label>address</Form.Label>
+							<Form.Control type="text" placeholder="enter address..."
+														onChange={(evt) => setAddress(evt.target.value)}/>
+						</Form.Group>
+						<Form.Group className="mb-3">
+							<Form.Label>pin code</Form.Label>
+							<Form.Control as="textarea" rows={3} placeholder="enter pin code..."
+														onChange={(evt) => setPinCode(evt.target.value)}/>
+						</Form.Group>
+						<Form.Group className="mb-3">
+							<Form.Label>phone number</Form.Label>
+							<Form.Control type="tel" placeholder="enter phone number..."
+														onChange={(evt) => setPhoneNumber(evt.target.value)}/>
+						</Form.Group>
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
+					</Button>
+					{loading ? <Button variant="primary" disabled>
+						<Spinner
+							as="span"
+							animation="border"
+							size="sm"
+							role="status"
+							aria-hidden="true"
+							className="me-2"
+						/>
+						Loading...
+					</Button> : <Button variant="primary" onClick={handlePlaceOrder}>
+						Submit order
+					</Button>}
+				</Modal.Footer>
+			</Modal>
+			<ToastContainer position="bottom-end" className="pe-3 mb-3">
+				{showToasts.show &&
+					<Toast className="bg-opacity-10" onClose={() => setShowToast({show: false})} show={showToasts.show}
+								 delay={10000} autohide
+								 bg={showToasts.type === "success" ? "success" : "danger"}>
+						<Toast.Header>
+							{showToasts.type === "success" ? <BsCheck2Circle/> : <BsExclamationCircle/>}
+							<strong className="ms-2 me-auto">{showToasts.title}</strong>
+							<small>just now</small>
+						</Toast.Header>
+						<Toast.Body>{showToasts.message}</Toast.Body>
+					</Toast>
+				}
+			</ToastContainer>
 		</Layout>
 	);
 };
