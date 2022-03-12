4f7e93905bbc46dda8e8ba1251294619d3e59d19
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2022-03-12 22:05:51 +0300

    # Home Page - added search and filter by category fields

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,17 @@
 import React, {useEffect, useState} from 'react';
-import {Button, Card, Col, Row} from "react-bootstrap";
+import {Button, Card, Col, Form, Image, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import {collection, getDocs} from "firebase/firestore";
 import firebaseDB from "../firebase.config";
 import Layout from "../components/Layout";
 import {useDispatch, useSelector} from "react-redux";
+import EmptyDataIcon from "../assets/img/empty-icon.png";
 
 const HomePage = () => {
 	const [products, setProducts] = useState([]);
 	const [loading, setLoading] = useState(false);
+	const [searchKey, setSearchKey] = useState('');
+	const [categoryFilter, setCategoryFilter] = useState('');
 	const {cartItems} = useSelector(state => state.cartReducer);
 	const dispatch = useDispatch();
 
@@ -44,26 +47,44 @@ const HomePage = () => {
 
 	return (
 		<Layout loading={loading}>
+			<Card className="mb-3">
+				<Card.Body>
+					<div className="d-flex w-50 gap-3">
+						<Form.Control type="text" placeholder="search products..." value={searchKey}
+													onChange={e => setSearchKey(e.target.value)}/>
+						<Form.Select aria-label="categories filter" value={categoryFilter}
+												 onChange={e => setCategoryFilter(e.target.value)}>
+							<option value="">all categories</option>
+							<option value="home & office">home & office</option>
+							<option value="health & beauty">health & beauty</option>
+							<option value="supermarket">supermarket</option>
+							<option value="phones & tablets">phones & tablets</option>
+							<option value="electronics">electronics</option>
+							<option value="fashion">fashion</option>
+						</Form.Select>
+					</div>
+				</Card.Body>
+			</Card>
 			<Row xs={1} md={2} lg={3} xl={4} className="g-3">
-				{products.map((p, idx) => (<Col key={p.id}>
-					<Card className="h-100">
-						<Link to={`/productinfo/${p.id}`} className="text-decoration-none text-dark">
-							<Card.Img variant="top" src={p.imageUrl} height={200} style={{objectFit: "contain"}}/>
-							<Card.Body>
-								<Card.Title>{p.name}</Card.Title>
-								<Card.Text className="mb-5">
-									This is a longer card with supporting text below as a natural
-									lead-in to additional content. This content is a little bit longer.
-								</Card.Text>
-							</Card.Body>
-						</Link>
-						<Card.Footer className="bg-transparent border-0">
-							<div className="position-absolute bottom-0 mb-3 d-flex gap-2">
-								<Button onClick={() => addToCart(p)}>add to cart</Button>
-							</div>
-						</Card.Footer>
-					</Card>
-				</Col>))}
+				{
+					products
+						.filter(p => p.name.toLowerCase().includes(searchKey))
+						.filter(p => p.category.toLowerCase().includes(categoryFilter))
+						.map((p, idx) => (<Col key={p.id}>
+							<Card className="h-100">
+								<Link to={`/productinfo/${p.id}`} className="text-decoration-none text-dark">
+									<Card.Body>
+										<Card.Img variant="top" src={p.imageUrl} height={150} style={{objectFit: "contain"}}/>
+										<Card.Title className="mb-5 mt-3">{p.name}</Card.Title>
+									</Card.Body>
+								</Link>
+								<Card.Footer className="bg-transparent border-0">
+									<div className="position-absolute bottom-0 mb-3 d-flex gap-2">
+										<Button onClick={() => addToCart(p)}>add to cart</Button>
+									</div>
+								</Card.Footer>
+							</Card>
+						</Col>))}
 			</Row>
 		</Layout>);
 };
